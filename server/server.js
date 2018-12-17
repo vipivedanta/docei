4ad8ba61158e87698cec94e2eb44c27a5c1bd2c1
Vipin Raghavan <vipin.kumar@katzion.com>
2018-12-17 10:50:04 +0530

    basic express js settings

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
+app.listen(port,function(){
+	console.log('Started');
+});
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/login',function(req,res){
+	res.sendFile(path.join(__dirname,'public','users/login.html'));
+});
